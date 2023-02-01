962454730fceda6aab61ae102dbcda1467a487e7
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2023-02-01 19:53:53 +0530

    final changes

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-let finalData;
-let text1;
-let getQuoteButton = document.getElementById('getQuoteButton');
-let shareWhatsapp = document.getElementById('shareWhatsapp');
-let quote = document.getElementById('quote');
-let author = document.getElementById('author');
-
-const getQuotes = async () => {
-   const url = `https://type.fit/api/quotes`;
-
-   try {
-      const data = await fetch(url);
-      const readableData = await data.json();
-
-      return readableData;
-   } catch (err) {}
-};
-
-const getSingleQuote = async () => {
-   const randomQuoteIndex = Math.floor(Math.random() * 1643);
-   // console.log(finalData);
-   let singleQuote = finalData[randomQuoteIndex];
-   text1 = singleQuote.text;
-   quote.innerText = '"' + singleQuote.text + '"';
-   singleQuote.author !== null
-      ? (author.innerText = 'By. ' + singleQuote.author)
-      : (author.innerText = 'By. ' + 'Unknown');
-
-   return singleQuote;
-};
-
-function share() {
-   // console.log(text1);
-   window.open(`whatsapp://send?text=${text1}`);
-}
-
-getQuoteButton.addEventListener('click', function () {
-   getSingleQuote();
-});
-
-getQuotes()
-   .then((res) => {
-      finalData = res;
-      getSingleQuote();
-   })
-   .catch((err) => {
-      console.log(err);
+$(document).ready(function () {
+   $('#toggleNav').click(() => {
+      $('#navbarNav').slideToggle();
    });
+
+   let finalData;
+   let text1;
+   let getQuoteButton = document.getElementById('getQuoteButton');
+   let shareWhatsapp = document.getElementById('shareWhatsapp');
+   let quote = document.getElementById('quote');
+   let author = document.getElementById('author');
+
+   const getQuotes = async () => {
+      const url = `https://type.fit/api/quotes`;
+
+      try {
+         const data = await fetch(url);
+         const readableData = await data.json();
+
+         return readableData;
+      } catch (err) {}
+   };
+
+   const getSingleQuote = async () => {
+      const randomQuoteIndex = Math.floor(Math.random() * 1643);
+      // console.log(finalData);
+      let singleQuote = finalData[randomQuoteIndex];
+      text1 = singleQuote.text;
+      quote.innerText = '"' + singleQuote.text + '"';
+      singleQuote.author !== null
+         ? (author.innerText = 'By. ' + singleQuote.author)
+         : (author.innerText = 'By. ' + 'Unknown');
+
+      return singleQuote;
+   };
+
+   function share() {
+      // console.log(text1);
+      window.open(`whatsapp://send?text=${text1}`);
+   }
+
+   getQuoteButton.addEventListener('click', function () {
+      getSingleQuote();
+   });
+
+   getQuotes()
+      .then((res) => {
+         finalData = res;
+         getSingleQuote();
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+});
