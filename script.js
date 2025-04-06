cced3c7706aa152ddaeb84e991e209d6b99aadc5
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2025-04-06 12:29:25 +0530

    refactor: update quote fetching logic to use a random quote API

--- script.js
+++ script.js
@@ -41,21 +41,51 @@ const getSingleQuote = async () => {
   return singleQuote;
 };
 
+const getRandomQuote = async () => {
+  const url = "https://dummyjson.com/quotes/random";
+
+  try {
+    loader.innerHTML = "Loading...";
+    const data = await fetch(url);
+    const readableData = await data.json();
+
+    return readableData;
+  } catch (error) {}
+};
+
 function share() {
   // console.log(text1);
   window.open(`whatsapp://send?text=${text1}-${text2}`);
 }
 
 getQuoteButton.addEventListener("click", function () {
-  getSingleQuote();
+  handleRandomQuoteUpdate();
 });
 
-getQuotes()
-  .then((res) => {
-    finalData = res;
-    loader.innerHTML = "";
-    getSingleQuote();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// getQuotes()
+//   .then((res) => {
+//     finalData = res;
+//     loader.innerHTML = "";
+//     getSingleQuote();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+function handleRandomQuoteUpdate() {
+  getRandomQuote()
+    .then((res) => {
+      loader.innerHTML = "";
+      text1 = res.quote;
+      text2 = res.author;
+      quote.innerText = '"' + res.quote + '"';
+      res.author !== null
+        ? (author.innerText = "By. " + res.author)
+        : (author.innerText = "By. " + "Unknown");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+handleRandomQuoteUpdate();
